2af0c389b037cb56ac393d5416512b8a363a7b31
Fredrik Meyer <hrmeyer@gmail.com>
2017-12-27 20:40:42 +0100

    Add day 11

---
+++ 11-2.rkt
@@ -0,0 +1,53 @@
+#lang racket
+
+(define (inp->complex-list inp)
+  (map dir->cplx (string-split inp ","))
+  )
+
+(define (dir->cplx str)
+  (define (str->non-unit str)
+    (cond ((equal? str "ne")
+           (+ 1 (/ (+ 1 (sqrt -3)) 2)))
+          ((equal? str "n")
+           (sqrt -3))
+          ((equal? str "nw")
+           (+ -1 (/ (+ -1 (sqrt -3)) 2)))
+          ((equal? str "sw")
+           (+ -1 (/ (- -1 (sqrt -3)) 2)))
+          ((equal? str "s")
+           (* -1 (sqrt -3)))
+          ((equal? str "se")
+           (+ 1 (/ (- 1 (sqrt -3)) 2)))
+          )
+    )
+  (let* (
+         (cpx (str->non-unit str))
+         (mag (magnitude cpx))
+         )
+    (/ cpx mag)
+    )
+  )
+
+
+(define (greedy-direction start goal)
+  (let (
+        (alts (list  "ne" "n" "nw" "sw" "s" "se"))
+        )
+    (argmin (lambda (d) (magnitude (- goal (+ start (dir->cplx d))))) alts)
+    )
+  )
+
+(define (number-of-steps start goal)
+  (if (< (magnitude (- start goal)) 0.5)
+      0
+      (+ 1 (number-of-steps (+ start (dir->cplx (greedy-direction start goal))) goal))
+      )
+  )
+
+(define (file->commands filename)
+  (string-split (string-trim (file->string filename)) ",")
+  )
+
+(number-of-steps 0 (apply + (map dir->cplx (list "se" "sw" "se" "sw" "sw"))))
+;; Answer
+;(number-of-steps 0 (apply + (map dir->cplx (file->commands "input11.txt"))))
